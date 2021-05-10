f4bd34c3d5e5b0ec21b9dc7fa45eb38571f252cc
Nathan <nmalcolm@users.noreply.github.com>
2021-05-10 22:19:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ apt-get install pdns-recursor wget unattended-upgrades vnstat vnstati apt-transp
 # Install Tor
 
 echo "deb https://deb.torproject.org/torproject.org `lsb_release -cs` main" \
-    | sudo tee /etc/apt/sources.list.d/torproject.list
+    | tee /etc/apt/sources.list.d/torproject.list
 
 wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
